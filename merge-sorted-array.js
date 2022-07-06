1990bc669163ee32c02825ecf113c5c82210311b
Jain <bishal.jain@rakuten.com>
2022-07-06 10:15:38 +0530

    merge tow sorted array

---
+++ merge-sorted-array.js
@@ -0,0 +1,12 @@
+const mergesortedArray = (nums1, nums2) => {
+  while (n > 0) {
+    if (nums1[m - 1] > nums2[n - 1]) {
+      nums1[m + n - 1] = nums1[m - 1];
+      m--;
+    } else {
+      nums1[m + n - 1] = nums2[n - 1];
+      n--;
+    }
+  }
+  return nums1;
+};
